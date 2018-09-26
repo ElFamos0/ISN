a4fdfea6d87dbfa5d44b4e8e3447dc3d9dd24508
ElFamosso <40206612+ElFamosso@users.noreply.github.com>
2018-09-27 00:32:20 +0200

    Delete Fiche1Ex3.py

--- Fiche1Ex3.py
+++
@@ -1,7 +0,0 @@
-﻿from math import sqrt
-xa = int(input("Coordonnée xA"))
-ya = int(input("Coordonnée yA"))
-xb = int(input("Coordonnée xB"))
-yb = int(input("Coordonnée yB"))
-AB = sqrt((xb-xa)**2 + (yb-ya)**2)
-print (AB)
