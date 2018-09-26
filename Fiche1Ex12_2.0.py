814a0e0897138888ee72ddaf2f3b6a9576e565fe
ElFamosso <40206612+ElFamosso@users.noreply.github.com>
2018-09-26 14:55:04 +0200

    Update Fiche1Ex12_2.0.py

--- Fiche1Ex12_2.0.py
+++ Fiche1Ex12_2.0.py
@@ -5,8 +5,9 @@ mot2 = mot
 
 for j in range (len(mot)):
     i = randint(j,len(mot)-1)
-    a = mot2[j]
-    b = mot2[i]
-    mot2 = mot2[0:j]+b+mot2[j+1:i]+a+mot2[i+1:]
+    if i==j:
+        a = mot2[j]
+        b = mot2[i]
+        mot2 = mot2[0:j]+b+mot2[j+1:i]+a+mot2[i+1:]
 
 print(mot2)
