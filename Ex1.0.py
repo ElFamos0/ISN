16983649a8b2eedfe966df94a04c39c8b1fbb610
ElFamosso <40206612+ElFamosso@users.noreply.github.com>
2018-09-27 00:34:22 +0200

    Add files via upload

---
+++ Ex1.0.py
@@ -0,0 +1,3 @@
+﻿a = int(input("Entrer un nombre"))
+carre = a**2
+print("Le carré de",a,"est",carre)
---
+++ Ex1.02.py
@@ -0,0 +1,13 @@
+﻿xa = int(input("xa"))
+ya = int(input("ya"))
+xb = int(input("xb"))
+yb = int(input("yb"))
+xi = (xa + xb)/2
+yi = (ya + yb)/2
+print("Coordonnés I (",xi,";",yi,")")
+if ya != yb:
+    print("Coupe l'axe des abscisse")
+    xm = xa - (ya*(xb-xa))/(yb-ya)
+    print("les coordonnés du point d'intersection est (",xm,"; 0)")
+else :
+    print("coupe pas l'axe des abscisse")
