1ba8e6ccbdb3cc0059ba0e4b4437bac0933b3c4d
El Famoso <40206612+ElFamosso@users.noreply.github.com>
2018-10-09 19:14:00 +0200

    Update Série 2/1.py

--- Série 2/1.py
+++ Série 2/1.py
@@ -1,6 +1,5 @@
 # -*- coding : utf8 -*-
 nbr = int(input("Entrer un nombre : "))
-
 while nbr<1 or nbr>3:
 	nbr = int(input("Entrer un nombre : "))
 print("Enfin vous avez décidé de choisir le bon nombre")
