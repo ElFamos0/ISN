e3f57a4070f44f908d34b39218e3c20ffe98742e
El Famoso <40206612+ElFamosso@users.noreply.github.com>
2018-10-18 23:20:21 +0200

    Python

---
+++ Exercice Python Perso/1.17 Farhenheit en Celsius.py
@@ -0,0 +1,18 @@
+# -*- coding : utf8 -*-
+
+def ConvertirenFahrhenheit(c):
+	Fahrhenheit = round((c*1.8)+32,1)
+	return Fahrhenheit
+
+def ConvertirenCelsius(f):
+	Celsius = round((f-32)/1.8,1)
+	return Celsius
+
+print("Convertisseur de température \nChoississez votre mode \n 0 : Celsius en Fahrhenheit \n 1 : Fahrhenheit en Celsius")
+a = int(input("Votre choix : "))
+if a == 0:
+	c = eval(input("Rentrer votre température en Celsius : " ))
+	print(ConvertirenFahrhenheit(c))
+elif a == 1:
+	f = eval(input("Rentrer votre température en Fahrhenheit : " ))
+	print(ConvertirenCelsius(f)) 
