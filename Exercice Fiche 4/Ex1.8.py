ad5271a2892e2c28c2fb13830cde41befbd7d179
El Famoso <40206612+ElFamosso@users.noreply.github.com>
2018-11-05 20:05:26 +0100

    Python

--- Exercice Fiche 4/Ex1.8.py
+++ Exercice Fiche 4/Ex1.8.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*
 
+'''
+	Auteur : Malo
+'''
+
+
+
 def conversionheure(h,m,s):
+	"Fonction qui fait une conversion d'un temps donné en heure, minute, seconde en seconde"
   h = 3600*h
   m = 60*m
   return(h+m+s)
