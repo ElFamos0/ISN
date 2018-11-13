8ab4fd908b1abaf617df97d13dd7680fb78b9761
ElFamosso <40206612+ElFamosso@users.noreply.github.com>
2018-11-13 23:49:27 +0100

    python

--- Bitconnect by Malo/bitconnect.py
+++ Bitconnect by Malo/bitconnect.py
@@ -11,23 +11,26 @@ pygame.init()
 def say_bitconnect():
     son.play()
 
-
 # Caractéristiques de la fenètre
 fen = Tk()
 fen.title("BITCONNNNEEEECTTTT")
 fen.configure(bg="black")
 
 # Integration de l'image so bitconnect
-photo = PhotoImage(file='bitconnect.gif')
+photo = PhotoImage(file='titlescreen.png')
+
 label = Label(fen, image=photo)
-label.pack()
+label.grid(row=1, column=1, columnspan=2)
+
 
 # Le petit bouton de la muerte    si tu lis ça oskar tu es une petite salope <3
 Bouton = Button(text="BITCOOONNNEEECTTT", bg="orange", command=say_bitconnect)
-Bouton.pack()
+Bouton.grid(row=2, column=1, sticky="W")
+Bouton2 = Button(text="chickens", bg="red")
+Bouton2.grid(row=2, column=2, sticky="E")
 
 # Le doux son a tes oreilles josé
-son = pygame.mixer.Sound("BITCONNECT.wav")
-
+son = pygame.mixer.Sound("SONS\\BITCONNECT.wav")
+son2 = pygame.mixer.Sound("SONS")
 
 fen.mainloop()
---
+++ CAT_MEME/CAT MEMES.py
@@ -0,0 +1,51 @@
+﻿# coding: utf-8
+# auteur: Oskar STAUDTE
+
+# modules utilisés:
+from tkinter import*
+
+
+# fonctions qui definissent les differentes memes de chat à afficher
+
+
+def ollie():
+    photo0 = PhotoImage(file='seriouscat.gif')
+    label.configure(image=photo0)
+    label.image = photo0
+
+
+def bongo():
+    photo1 = PhotoImage(file='bongo.gif')
+    label.configure(image=photo1)
+    label.image = photo1
+
+
+def longcat():
+    photo2 = PhotoImage(file='longcat.gif')
+    label.configure(image=photo2)
+    label.image = photo2
+
+# definiton de la fonction menu
+
+
+
+# fenetre principale
+fen = Tk()
+fen.title("ObscureCatMemes")
+fen.configure(bg="black")
+
+photo = PhotoImage(file="seriouscat.gif")
+label = Label(fen, image=photo)
+label.pack()
+
+b1 = Button(text="Polite cat", bg="Yellow", fg="Black", command=ollie)
+b1.pack()
+
+b2 = Button(text="Bongo cat", bg="Yellow", fg="Black", command=bongo)
+b2.pack()
+
+b3 = Button(text="Long cat", bg="Yellow", fg="Black", command=longcat)
+b3.pack()
+
+
+fen.mainloop()
